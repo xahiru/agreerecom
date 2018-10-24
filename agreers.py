9a743484f6f3f317a35f7a3d7d971423fbbcb4c7
Ahmed Zahir <zahir@Ahmeds-MacBook-Pro.local>
2018-10-24 16:14:06 +0800

    trust matrix done, needs optimization

--- agreers.py
+++ agreers.py
@@ -3,6 +3,7 @@ import math
 from math import sqrt
 import numpy as np
 from math import ceil
+import time
 np.seterr(divide='ignore', invalid='ignore')
 
 #   -- Scikit Learn --
@@ -13,6 +14,8 @@ from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.model_selection import train_test_split
 ##  Tool for calculating MSE
 from sklearn.metrics import mean_squared_error
+
+import matplotlib.pyplot as plt
  
 # UserID::MovieID::Rating::Timestamp
 # UserID::Gender::Age::Occupation::Zip-code
@@ -22,7 +25,7 @@ user_data = pd.read_table('data/ml-1m/ratings.dat', sep='::', names=['user id', 
 # user_user = pd.read_table('data/ml-1m/users.dat', sep='::',names=['user id', 'age', 'gender', 'occupation', 'zip code'])
 # movies_list = pd.read_table('data/ml-1m/movies.dat', sep='::',names=['movie id', 'tite','genre' ])
 
-user_data = user_data.head(100)
+# user_data = user_data.head(300000)
 
 n_users = user_data['user id'].unique().shape[0]
 n_items = user_data['item id'].unique().shape[0]
@@ -51,12 +54,12 @@ for line in user_data.itertuples():
     rating_matrix[line[1]-1, line[2]-1] = line[3]
 
 
-print(rating_matrix[0:10])
+# print(rating_matrix[0:10])
 trust_matix = np.zeros((n_users,n_users))
 def agreement(ratings, alpha):
     #for each unique user iterate
     for user_a in range(n_users):
-        for user_b in range(n_users):
+        for user_b in range(user_a,n_users):
             if user_a != user_b:
                 a_ratings = rating_matrix[user_a]
                 b_ratings = rating_matrix[user_b]
@@ -64,38 +67,69 @@ def agreement(ratings, alpha):
                 a_ix = np.nonzero(a_ratings)
                 b_ix = np.nonzero(b_ratings)
 
-                #get the non zero values
-                # np.nonzero(a > 3)
-                # commonset = set(a_ix).intersection(b_ix)
+                # commonset = np.intersect1d(np.nonzero(rating_matrix[user_a]), np.nonzero(rating_matrix[user_b]))
                 commonset = np.intersect1d(a_ix, b_ix)
 
-                if user_a <10 and user_b <10:
-                    print(commonset)
-                    print('a_ratings[commonset]')
-                    print(a_ratings[commonset])
-                    print('b_ratings[commonset]')
-                    print(b_ratings[commonset])
+                    # print(commonset)
+                    # print('a_ratings[commonset]')
+                    # print(a_ratings[commonset])
+                    # print('b_ratings[commonset]')
+                    # print(b_ratings[commonset])
+
+                    # print('len(commonset)')
+                    
+                common_set_length = len(commonset)
+                # print(common_set_length)
+                # print(user_a)
                 
 
 
 
                 #get #of common positive n common negatives
 
+
+                trust = 0
+
+                if(common_set_length > 0):
                     a_positive = a_ratings[commonset] > alpha
                     b_positive = b_ratings[commonset] > alpha
 
-                    agreed_positive = np.logical_and(a_positive, b_positive)
+                        # 5,1,5,4,2,2 >2.4 = T,F,T,T,F,F
+                        #                               =(po1)+(ne2) = (3) 
+                        # 1,5,1,3,1,1 >2.4 = F,T,F,T,F,F   
 
-                    # print(agreed_positive)
+ 
+                        # print("a_positive")
+                        # print(a_positive)
+
+                        # print("b_positive")
+                        # print(b_positive)
+
+                    agreed_positive = np.logical_and(a_positive, b_positive)
+                        # print("agreed_positive")
+                        # print(agreed_positive)
 
                     a_negative = a_ratings[commonset] < alpha
                     b_negative = b_ratings[commonset] < alpha
 
-                    agreed_negative = np.logical_and(a_positive, b_positive)
+                        # print("a_negative")
+                        # print(a_negative)
+
+                        # print("b_negative")
+                        # print(b_negative)
+
+                    agreed_negative = np.logical_and(a_negative, b_negative)
+                        # print("agreed_negative")
+                        # print(agreed_negative)
+
                     agreement = np.sum(agreed_positive) + np.sum(agreed_negative)
 
-                    trust = agreement/len(commonset)
-                    trust_matix[user_a,user_b] = trust
+                        # print('agreement')
+                        # print(agreement)
+                        
+                    trust = agreement/common_set_length
+
+                trust_matix[user_a,user_b] = trust
 
                 # trust = total agreement/total ratings
                 # total agreement = agreed (positives + negatives)
@@ -112,7 +146,18 @@ def agreement(ratings, alpha):
 
 
 
-
+start = time.time()
 agreement(rating_matrix, 2.5)
-
+total = start - time.time()
+print(total)
 print(trust_matix)
+
+# plt.imshow(trust_matix);
+# plt.colorbar()
+# plt.show()
+
+plt.matshow(trust_matix);
+plt.colorbar()
+plt.show()
+
+# np.save('trust_matix.npy', trust_matix)
