782eeda9ace6d2181f6b3259fe996862066893c5
xahiru <xahiru@gmail.com>
2018-12-24 20:27:50 +0800

    pytoch added

--- agreetrust.py
+++ agreetrust.py
@@ -1,5 +1,8 @@
 from __future__ import (absolute_import, division, print_function,             
-                        unicode_literals)                                      
+                        unicode_literals)
+from __future__ import print_function
+import torch
+                                      
 import pickle
 import os
 
