e4658fb23b9eca4858f2761e1300cd0aef53ad3f
xahiru <xahiru@gmail.com>
2018-12-26 18:29:11 +0800

    os govind snippet addedd

--- govind.py
+++ govind.py
@@ -3,6 +3,12 @@ from surprise import KNNWithMeans
 from surprise.model_selection import train_test_split
 from surprise.accuracy import rmse
 start=time.time()
+
+import platform; print(platform.platform())
+import sys; print("Python", sys.version)
+import surprise; print("surprise", surprise.__version__)
+
+
 print(start)
 data = Dataset.load_builtin('jester')
 trainset, testset = train_test_split(data, test_size=.2, train_size=None, random_state=100, shuffle=True)
