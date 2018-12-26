535956f2fd9fc9407b386772b9c9fd117acfd164
xahiru <xahiru@gmail.com>
2018-12-26 18:26:20 +0800

    govind test case addedd

--- govind.py
+++ govind.py
@@ -1,11 +1,7 @@
-from surprise import Dataset, evaluate
+from surprise import Dataset
 from surprise import KNNWithMeans
-from collections import defaultdict
 from surprise.model_selection import train_test_split
 from surprise.accuracy import rmse
-from surprise.accuracy import mae
-import os
-import time
 start=time.time()
 print(start)
 data = Dataset.load_builtin('jester')
@@ -15,5 +11,4 @@ algo = KNNWithMeans(sim_options=sim_options,verbose=True)
 algo.fit(trainset)
 predictions=algo.test(testset)
 rmse(predictions)
-mae(predictions)
 print("done")
