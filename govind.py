131564c4857c3e75e0914999bbc32f185dfd301b
xahiru <xahiru@gmail.com>
2018-12-26 18:32:05 +0800

    oscl addedd

--- govind.py
+++ govind.py
@@ -2,14 +2,11 @@ from surprise import Dataset
 from surprise import KNNWithMeans
 from surprise.model_selection import train_test_split
 from surprise.accuracy import rmse
-start=time.time()
 
 import platform; print(platform.platform())
 import sys; print("Python", sys.version)
 import surprise; print("surprise", surprise.__version__)
 
-
-print(start)
 data = Dataset.load_builtin('jester')
 trainset, testset = train_test_split(data, test_size=.2, train_size=None, random_state=100, shuffle=True)
 sim_options={'name':'pearson','user_based':True}
@@ -17,4 +14,3 @@ algo = KNNWithMeans(sim_options=sim_options,verbose=True)
 algo.fit(trainset)
 predictions=algo.test(testset)
 rmse(predictions)
-print("done")
